de2115c5b88e362d9611dca3b6030ff39b0b629f
Brxno1 <1reddw1@example.com>
2025-10-03 12:28:56 -0300

    chore(repo): housekeeping and docker adjustments
    - update .gitignore, docker-compose.yml, pnpm-lock.yaml
    - adjust auth-service package and app files
    - remove app.controller.spec.ts

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './entities/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST!,
+      port: parseInt(process.env.DB_PORT!),
+      username: process.env.DB_USERNAME!,
+      password: process.env.DB_PASSWORD!,
+      database: process.env.DB_NAME!,
+      entities: [User],
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/auth-service/src/app.service.ts
+++ apps/auth-service/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor() { }
+
+  async getHealth(): Promise<{ status: string }> {
+    return {
+      status: 'healthy',
+    }
   }
 }
